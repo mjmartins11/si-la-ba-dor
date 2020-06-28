3b69eb189499dcc1b854c9fbc707b9bc864856c7
mjmartins11 <48074706+mjmartins11@users.noreply.github.com>
2020-06-28 12:42:30 -0300

    Clicar no botão e aparecer a sílaba
    Foi implementado a atualização da JFrame quando seleciona um botão para aparecer uma sílaba.

--- workspace/si-la-ba-dor/src/view/Jogo.java
+++ workspace/si-la-ba-dor/src/view/Jogo.java
@@ -5,8 +5,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -14,7 +18,7 @@ import java.awt.Color;
 public class Jogo extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel contentPane = new JPanel();
 
 	private final int ESPACAMENTO = 200;
 	
@@ -26,17 +30,15 @@ public class Jogo extends JFrame {
 		setBounds(100, 100, 450, 363);	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel background;
 		setSize(1280, 720);
-		contentPane = new JPanel();
+		
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		ImageIcon img = new ImageIcon("assets/background_silabador.png");
-
 		
+		//ImageIcon img = new ImageIcon("assets/background_silabador.png");
+
 		JLabel lblNivel = new JLabel("Nivel: " + (nivel + 1));
 		lblNivel.setForeground(new Color(0, 0, 128));
 		lblNivel.setFont(new Font("Cooper Black", Font.PLAIN, 20));
@@ -48,7 +50,7 @@ public class Jogo extends JFrame {
 		lblJogador.setFont(new Font("Cooper Black", Font.PLAIN, 50));
 		lblJogador.setBounds(277, 72, 747, 71);
 		contentPane.add(lblJogador);
-		
+				
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -61,55 +63,52 @@ public class Jogo extends JFrame {
 		contentPane.add(btnVoltar);
 		
 		JButton btnSilaba = new JButton("PRO");
-		btnSilaba.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selecionarSilaba(btnSilaba.getText());
-			}
-		});
+		
 		btnSilaba.setBackground(new Color(0, 0, 128));
 		btnSilaba.setForeground(new Color(255, 204, 102));
 		btnSilaba.setFont(new Font("Cooper Black", Font.PLAIN, 40));
 		btnSilaba.setBounds(178, 199, 141, 89);
-		contentPane.add(btnSilaba);				
-		background = new JLabel("", img,JLabel.CENTER);
-		background.setBackground(new Color(51, 204, 204));
-		background.setBounds(0, 0, 1262, 673);
-		getContentPane().add(background);
+		contentPane.add(btnSilaba);
+		
+		btnSilaba.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selecionarSilaba(btnSilaba.getText());
+				repaint();
+			}
+		});
 	}
 	
 	private void adicionarSilaba(String silaba) {
 		JLabel lblSilaba = new JLabel(silaba);
 		lblSilaba.setForeground(Color.ORANGE);
-		lblSilaba.setFont(new Font("Cooper Black", Font.PLAIN, 65));
-		lblSilaba.setBounds(xProximaSilaba, 586, 348, 55);
+		lblSilaba.setFont(new Font("Cooper Black", Font.PLAIN, 32));
+		lblSilaba.setBounds(xProximaSilaba+7, 586, 348, 55);
 		contentPane.add(lblSilaba);
 		this.xProximaSilaba = this.xProximaSilaba + ESPACAMENTO;
 	}
 	
 	private void adicionarBarra(int quantidadeDeLetras) {
-		JLabel lblBarra;
+		JLabel lblBarra = new JLabel();
 		switch(quantidadeDeLetras) {
 			case 1:
-				lblBarra = new JLabel("_");
+				lblBarra.setText("__");
 				break;
 			case 2:
-				lblBarra = new JLabel("__");
+				lblBarra.setText("____");
 				break;
 			case 3:
-				lblBarra = new JLabel("___");
+				lblBarra.setText("______");
 				break;
 			default:
-				lblBarra = new JLabel("____");
-				
+				lblBarra.setText("________");
 		}
 		
 		lblBarra.setForeground(Color.BLACK);
 		lblBarra.setFont(new Font("Dialog", Font.PLAIN, 25));
-		lblBarra.setBounds(xProximaBarra, 528, 997, 141);
+		lblBarra.setBounds(xProximaBarra, 550, 997, 141);
 		contentPane.add(lblBarra);
 		this.xProximaBarra = this.xProximaBarra + ESPACAMENTO;
 	}
-	
 	
 	private void selecionarSilaba(String texto) {
 		adicionarSilaba(texto);
